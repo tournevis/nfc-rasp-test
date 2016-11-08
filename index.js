cbd7cb6a071f992a8da2f2725e10c92dd32e556a
tournevis <arthur.ro92@gmail.com>
2016-11-08 12:12:28 +0100

    📢

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ console.log('nfc.version(): ' + util.inspect(nfc.version(), { depth: null }))
     // { name: 'libfnc', version: '1.7.0' }
 console.log('nfc.scan(): ' + util.inspect(nfc.scan(), { depth: null }))
     // { 'pn53x_usb:160:012': { name: 'SCM Micro / SCL3711-NFC&RW', info: { chip: 'PN533 v2.7', ... } } }
-let device = new nfc.NFC()
+var device = new nfc.NFC()
 
 device.on('read', tag => {
   if ((!!tag.data) && (!!tag.offset)) console.log(util.inspect(nfc.parse(tag.data.slice(tag.offset)), { depth: null }))
